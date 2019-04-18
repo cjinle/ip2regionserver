4a7df669432e18b07434e44aa55d699ebc6edb63
cjinle <cjinle@gmail.com>
2019-04-18 17:27:20 +0800

    fix

--- main2.go
+++ main2.go
@@ -8,6 +8,11 @@ import (
 	"github.com/lionsoul2014/ip2region/binding/golang/ip2region"
 )
 
+type Stat struct {
+	Succ int
+	Fail int
+}
+
 func main() {
 	fmt.Println("http server")
 	region, err := ip2region.New("conf/ip2region.db")
@@ -15,6 +20,7 @@ func main() {
 		panic("ip2region.db err")
 	}
 	defer region.Close()
+	stat := Stat{0, 0}
 
 	http.HandleFunc("/query", func(w http.ResponseWriter, r *http.Request) {
 		ip := r.URL.Query().Get("ip")
@@ -24,14 +30,20 @@ func main() {
 		ipInfo, err := region.MemorySearch(ip)
 		if err != nil {
 			fmt.Fprintf(w, "[]")
+			stat.Fail = stat.Fail + 1
 		} else {
 			b, err := json.Marshal(ipInfo)
 			if err != nil {
 				b = []byte("[]")
 			}
+			stat.Succ = stat.Succ + 1
 			fmt.Fprintf(w, string(b))
 		}
 
 	})
-	http.ListenAndServe(":8082", nil)
+	http.HandleFunc("/stat", func(w http.ResponseWriter, r *http.Request) {
+		b, _ := json.Marshal(stat)
+		fmt.Fprintf(w, string(b))
+	})
+	http.ListenAndServe(":8080", nil)
 }
